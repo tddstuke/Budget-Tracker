cef510da295979d0ec2b8f3262796ecbf29f2920
Todd Stuke <tddstuke@gmail.com>
2022-07-23 13:25:09 -0500

    updated indexDB to send offline data

--- public/js/idb.js
+++ public/js/idb.js
@@ -7,9 +7,9 @@ request.onupgradeneeded = function (event) {
   const db = event.target.result;
   // create an object store (table) called `new_pizza`, set it to have an auto incrementing primary key of sorts
   db.createObjectStore("budget_item", { autoIncrement: true });
-  //   upon a successful
 };
 
+//   upon a successful
 request.onsuccess = function (event) {
   db = event.target.result;
 
@@ -34,3 +34,40 @@ function saveRecord(record) {
   // add record to your store with add method
   budgetObjectStore.add(record);
 }
+
+function uploadBudget() {
+  const transaction = db.transaction(["budget_item"], "readwrite");
+
+  const budgetObjectStore = transaction.objectStore("budget_item");
+
+  const getAll = budgetObjectStore.getAll();
+
+  getAll.onsuccess = function () {
+    if (getAll.result.length > 0) {
+      fetch("/api/transaction/bulk", {
+        method: "POST",
+        body: JSON.stringify(getAll.result),
+        headers: {
+          Accept: "application/json, text/plain, */*",
+          "Content-Type": "application/json",
+        },
+      })
+        .then((response) => response.json())
+        .then((serverResponse) => {
+          if (serverResponse.message) {
+            throw new Error(serverResponse);
+          }
+          const transaction = db.transaction(["budget_item"], "readwrite");
+
+          const budgetObjectStore = transaction.objectStore("budget_item");
+
+          budgetObjectStore.clear();
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    }
+  };
+}
+
+window.addEventListener("online", uploadBudget);
